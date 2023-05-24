bd8d946e65d204991836733b592ad10e747dee0f
goonadutch <goonadutch@gmail.com>
2023-05-24 15:17:11 +0530

    base commit

---
+++ app.js
@@ -0,0 +1,23 @@
+require('dotenv').config()
+const express = require("express")
+const app = express()
+const port = process.env.PORT || 5000;
+const path = require('path')
+const mongoose = require("./database")
+
+const server = app.listen(port, () => console.log("Server Listening on " + port));
+
+app.set("view engine", "pug");
+app.set("views", "views");
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json())
+app.use(express.static(path.join(__dirname, "public")));
+
+// Routes
+const homeRoute = require("./routes/homeRoutes");
+const searchRoute = require("./routes/searchRoutes");
+
+app.use("/", homeRoute);
+app.use("/home", homeRoute);
+app.use("/search", searchRoute);
